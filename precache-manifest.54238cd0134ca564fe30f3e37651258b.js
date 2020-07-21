f8665ea665b12e257a4e533f8334025b3b6a8964
react_try_3.0 <shatilovvlad@mail.ru>
2020-07-21 18:01:24 +0300

    Updates

---
+++ precache-manifest.54238cd0134ca564fe30f3e37651258b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a6eb02988cdc29fb46a4bdc6e00da30",
+    "url": "/react_first_try_/index.html"
+  },
+  {
+    "revision": "ac4097bdac19b5720f72",
+    "url": "/react_first_try_/static/css/main.8985bafb.chunk.css"
+  },
+  {
+    "revision": "b409dcd7a4109a46fcb7",
+    "url": "/react_first_try_/static/js/2.5b50e85f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_first_try_/static/js/2.5b50e85f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac4097bdac19b5720f72",
+    "url": "/react_first_try_/static/js/main.dd2bda4b.chunk.js"
+  },
+  {
+    "revision": "a719bf934373ee48e95c",
+    "url": "/react_first_try_/static/js/runtime-main.d3056c94.js"
+  }
+]);
